8322faa30a5563183d7e5cf91f48ab2ae1c2d2d7
Aadi Jain <258081268+Aadijain741749@users.noreply.github.com>
2026-06-12 16:20:53 +0530

    Add files via upload

---
+++ 2_for.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  for(int num = 1; num<=10; num++){
+    printf("%d\n", num);
+  }
+  printf("Loop has ended.\n");
+}
---
+++ 8_do_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int i = 1;
+  do {
+    printf("%d\n", i);
+    i++;
+  } while (i <= 10);
+
+  int num;
+  do{
+    printf("Please, enter the number: ");
+    scanf("%d", &num);
+  } while (num!= 10);
+  printf("Congrats, you enter the correct number.\n");
+  
+  return 0;
+}
